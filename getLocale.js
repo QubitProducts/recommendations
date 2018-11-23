e41114e4886d5e75c64f6543573a2210b2f79c92
Alexander Esp <alexjesp@gmail.com>
2018-11-23 09:44:58 +0000

    Don't restrict event types, don't descructure

--- getLocale.js
+++ getLocale.js
@@ -1,8 +1,8 @@
 module.exports = function getLocale (options) {
   return new Promise((resolve, reject) => {
     options.uv.on(/^([^.]+\.)?[a-z]{2}View$/, function (event) {
-      var { type, language, currency } = event
-      if (/^(basket|checkout|transaction)$/i.test(type)) return
+      var language = event.language
+      var currency = event.currency
       var currentLocale = language || currency || ''
       if (language && currency) {
         currentLocale = [language, currency].join('-')
